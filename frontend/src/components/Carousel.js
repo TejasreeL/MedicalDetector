207969e9a6b3f45552457a7667f3f17cf0adc8e0
bharath <bharathbharath6456@gamail.com>
2024-06-12 14:51:58 +0530

    3rd

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Carousel.css';
+
 
 export default function Carousel() {
     return (
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Carousel from '../components/Carousel'
-=======
-import React from "react";
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
->>>>>>> 2d8e6e01179031e02311d1f8648ce7379522aa93
 
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
         <Navbar/>
         <div>
         <Carousel/>
         </div>
-=======
-      <Navbar />
       <Footer />
->>>>>>> 2d8e6e01179031e02311d1f8648ce7379522aa93
     </div>
   );
 }
