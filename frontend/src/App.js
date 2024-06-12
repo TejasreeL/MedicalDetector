41f9a453832dda48833cbe47ec30721ac4285aa5
TejasreeL <tejasreelokireddy@gmail.com>
2024-06-12 15:26:22 +0530

    selectdiseases

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './screens/Home'
+import DiseasePredict from './screens/DiseasePredict';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/predict" element={<DiseasePredict/>}/>
         </Routes>
       </div>
     </Router>
